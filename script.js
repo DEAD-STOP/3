b0d4faa22f6b692803f2617f49fe2cc06b370c24
Austin Stoppenhagen <arstop98@gmail.com>
2022-09-13 15:17:07 -0500

    script workin on

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// const passLetters = 'abcdefghijklmnopqrstuvwxyz';
+const passLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ];
+const passLettersCaps = passLetters.map(x => x.toUpperCase());
+console.log(passLetters);
+console.log(passLettersCaps);
+const passNumbs = ['1','2','3','4','5','6','7','8','9','0',]
+const passSymbs = ['!','@','#','$','%','^','&','*','(',')','[',']','{','}','-','_','=','+',';',':','<','>',',','.','/','?',`'`,`"`,];
+const charArray = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +17,11 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+var generatePassword = () => {
+  let passArray = [];
+  return passArray.join('');
 }
 
 // Add event listener to generate button
